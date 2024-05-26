f52c4949dcddb9047fa9c8c70a83291955d0442f
AlexRosly <rosly.a777@gmail.com>
2024-05-26 16:13:46 +0300

    change nodemailer setup

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,19 +1,33 @@
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
 require("dotenv").config();
 
-const { NODEMAILER_PASS } = process.env;
+const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN } = process.env;
 
+const clientId = CLIENT_ID;
+const clientSecret = CLIENT_SECRET;
+const redirect_uri = "https://developers.google.com/oauthplayground";
+const EMAIL = "24serpnya.com.ua@gmail.com";
+
+const OAuth2Client = new google.auth.OAuth2({
+  clientId,
+  clientSecret,
+  redirect_uri,
+});
+
+OAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+const ACCESS_TOKEN = OAuth2Client.getAccessToken();
 const nodemailerConfig = {
-  //variant 1 for gmail
-
-  // service: "gmail",
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
-    user: "yourpricebooking@gmail.com", // from send email
-    pass: NODEMAILER_PASS,
+    type: "OAuth2",
+    user: EMAIL,
+    clientId,
+    clientSecret,
+    refreshToken: REFRESH_TOKEN,
+    accessToken: ACCESS_TOKEN,
   },
+  tls: { rejectUnauthorized: true },
 };
 
 const transporter = nodemailer.createTransport(nodemailerConfig);
