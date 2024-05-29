a9a594b34c2cb979d6f8cec87b0994a8a81cbfb2
AlexRosly <rosly.a777@gmail.com>
2024-05-29 13:14:02 +0300

    update email markup

--- middelwares/createRegistrationCode.js
+++ middelwares/createRegistrationCode.js
@@ -25,30 +25,15 @@ const createRegistrationCode = async (req, res) => {
   const validCode = createdCode.getTime() + 180000;
 
   const mail = {
-    form: "noreply@yourpricebooking.com",
+    form: "24serpnya.com.ua@gmail.com",
     to: email,
     subject: "Confirmation code",
     text: "Office of the Citizen confirmation code",
-    // html: `<p>Your confirmation code ${secretCode}.</p><br/><p>Attention code valid only 3 minutes</p>`,
-    // amp: `<!doctype html>
-    //   <html ⚡4email data-css-strict>
-    //   <head>
-    //     <meta charset="utf-8">
-    //     <script async src="https://cdn.ampproject.org/v0.js"></script>
-    //     <style amp4email-boilerplate>body{visibility:hidden}</style>
-    //   </head>
-    //   <body>
-    //           <p>Your confirmation code ${secretCode}.</p>
-    //           <p>Attention code valid only 3 minutes</p>
-    //   </body>
-    //   </html>`,
     html: `<!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>HTML Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600" rel="stylesheet" />
     <style>
@@ -82,8 +67,7 @@ const createRegistrationCode = async (req, res) => {
 
         table {
             border-collapse: collapse;
-            mso-table-lspace: 0pt;
-            mso-table-rspace: 0pt;
+            margin: auto;
         }
 
         .logo {
@@ -106,8 +90,8 @@ const createRegistrationCode = async (req, res) => {
             font-weight: 400;
             line-height: 14.39px;
             text-align: center;
-
-
+            padding-left: 5px;
+            padding-right: 5px;
         }
 
         .links:hover {
@@ -118,6 +102,15 @@ const createRegistrationCode = async (req, res) => {
         td {
             padding-top: 0px;
             padding-bottom: 0px;
+            text-align: center;
+        }
+
+        .contr {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            flex-wrap: nowrap;
         }
 
         .authorizationheder {
@@ -149,10 +142,7 @@ const createRegistrationCode = async (req, res) => {
         }
     </style>
 </head>
-
 <body style="margin: 0; padding: 0;">
-    <div style="font-size:0px;font-color:#ffffff;opacity:0;visibility:hidden;width:0;height:0;display:none;">Тестовое
-        письмо</div>
     <table cellpadding="0" cellspacing="0" max-width="100%" bgcolor="#fbfbfb" align="center">
         <tr>
             <td>
@@ -164,10 +154,10 @@ const createRegistrationCode = async (req, res) => {
                         <td height="52" max-width="547">
                             <table cellpadding="0" cellspacing="0" max-width="310" align="center">
                                 <tr>
-                                    <td class="logo" align="left" height="48" max-width="12">
+                                    <td class="logo" align="center" height="48" max-width="12">
                                         <img src="https://i.ibb.co/rG1wSjv/logo2.jpg" alt="logo" />
                                     </td>
-                                    <td class="logo" align="left" height="52" max-width="292">
+                                    <td class="logo" align="center" height="52" max-width="292">
                                         <img src="https://i.ibb.co/D1wbPxH/logo1.jpg" alt="logo" />
                                     </td>
                                 </tr>
@@ -184,7 +174,7 @@ const createRegistrationCode = async (req, res) => {
                                 </tr>
                                 <tr>
                                     <td class="authorization" align="center" height="48" max-width="12">
-                                        ${secretCode}
+                                        7856
                                     </td>
                                 </tr>
                                 <tr>
@@ -197,12 +187,17 @@ const createRegistrationCode = async (req, res) => {
                     </tr>
                     <tr>
                         <td height="130" max-width="407">
-                            <table cellpadding="0" cellspacing="0" max-width="407" align="center">
-                                <tr>
-                                    <td class="logo" align="center" height="48" max-width="12">
-                                        <a class="links" target="_blank"
-                                            href="https://www.support@24serpnya.com.ua">Підтримка користувачів</a>
+                            <table class="contr" cellpadding="0" cellspacing="0" max-width="407" text-align="center">
+                                <tr class="contr">
+                                    <td class="links logo" align="center" height="48" max-width="12">
+                                        Підтримка користувачів
                                     </td>
+                                    <td class="links logo" align="center" height="48" max-width="12">
+                                        support@24serpnya.com.ua
+                                    </td>
+                                </tr>
+
+                                <tr>
                                     <td class="logo" align="center" height="48" max-width="12">
                                         <a class="links" target="_blank" href="https://www.youtube.com/@24serpnya">Ми на
                                             You Tube</a>
@@ -210,8 +205,6 @@ const createRegistrationCode = async (req, res) => {
                                     <td class="logo" align="center" height="48" max-width="12">
                                         <a class="links" target="_blank" href="https://x.com/KabinetU60914">Ми в X</a>
                                     </td>
-                                </tr>
-                                <tr>
                                     <td class="logo" align="center" height="48" max-width="12">
                                         <a class="links" target="_blank"
                                             href="https://www.instagram.com/kabinet_gromadyanina">Ми в Instagram</a>
@@ -236,9 +229,7 @@ const createRegistrationCode = async (req, res) => {
         </tr>
     </table>
 </body>
-
-</html>
-`,
+</html>`,
   };
   const findCandidate = await Candidate.find({ email });
 
--- middelwares/createSignInCode.js
+++ middelwares/createSignInCode.js
@@ -34,30 +34,15 @@ const createSingInCode = async (req, res) => {
     });
   }
   const mail = {
-    form: "yourpricebooking@gmail.com",
+    form: "24serpnya.com.ua@gmail.com",
     to: email,
     subject: "Confirmation code",
-    text: "Your Price Booking confirmation code",
-    // html: `<p>Your confirmation code ${secretCode}.</p><br/><p>Attention code valid only 3 minutes</p>`,
-    // amp: `<!doctype html>
-    //   <html ⚡4email data-css-strict>
-    //   <head>
-    //     <meta charset="utf-8">
-    //     <script async src="https://cdn.ampproject.org/v0.js"></script>
-    //     <style amp4email-boilerplate>body{visibility:hidden}</style>
-    //   </head>
-    //   <body>
-    //           <p>Your confirmation code ${secretCode}.</p>
-    //           <p>Attention code valid only 3 minutes</p>
-    //   </body>
-    //   </html>`,
+    text: "Office of the Citizen confirmation code",
     html: `<!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>HTML Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600" rel="stylesheet" />
     <style>
@@ -91,8 +76,7 @@ const createSingInCode = async (req, res) => {
 
         table {
             border-collapse: collapse;
-            mso-table-lspace: 0pt;
-            mso-table-rspace: 0pt;
+            margin: auto;
         }
 
         .logo {
@@ -115,8 +99,8 @@ const createSingInCode = async (req, res) => {
             font-weight: 400;
             line-height: 14.39px;
             text-align: center;
-
-
+            padding-left: 5px;
+            padding-right: 5px;
         }
 
         .links:hover {
@@ -127,6 +111,15 @@ const createSingInCode = async (req, res) => {
         td {
             padding-top: 0px;
             padding-bottom: 0px;
+            text-align: center;
+        }
+
+        .contr {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            flex-wrap: nowrap;
         }
 
         .authorizationheder {
@@ -160,8 +153,6 @@ const createSingInCode = async (req, res) => {
 </head>
 
 <body style="margin: 0; padding: 0;">
-    <div style="font-size:0px;font-color:#ffffff;opacity:0;visibility:hidden;width:0;height:0;display:none;">Тестовое
-        письмо</div>
     <table cellpadding="0" cellspacing="0" max-width="100%" bgcolor="#fbfbfb" align="center">
         <tr>
             <td>
@@ -173,10 +164,10 @@ const createSingInCode = async (req, res) => {
                         <td height="52" max-width="547">
                             <table cellpadding="0" cellspacing="0" max-width="310" align="center">
                                 <tr>
-                                    <td class="logo" align="left" height="48" max-width="12">
+                                    <td class="logo" align="center" height="48" max-width="12">
                                         <img src="https://i.ibb.co/rG1wSjv/logo2.jpg" alt="logo" />
                                     </td>
-                                    <td class="logo" align="left" height="52" max-width="292">
+                                    <td class="logo" align="center" height="52" max-width="292">
                                         <img src="https://i.ibb.co/D1wbPxH/logo1.jpg" alt="logo" />
                                     </td>
                                 </tr>
@@ -193,7 +184,7 @@ const createSingInCode = async (req, res) => {
                                 </tr>
                                 <tr>
                                     <td class="authorization" align="center" height="48" max-width="12">
-                                        ${secretCode}
+                                        7856
                                     </td>
                                 </tr>
                                 <tr>
@@ -206,12 +197,17 @@ const createSingInCode = async (req, res) => {
                     </tr>
                     <tr>
                         <td height="130" max-width="407">
-                            <table cellpadding="0" cellspacing="0" max-width="407" align="center">
-                                <tr>
-                                    <td class="logo" align="center" height="48" max-width="12">
-                                        <a class="links" target="_blank"
-                                            href="https://www.support@24serpnya.com.ua">Підтримка користувачів</a>
+                            <table class="contr" cellpadding="0" cellspacing="0" max-width="407" text-align="center">
+                                <tr class="contr">
+                                    <td class="links logo" align="center" height="48" max-width="12">
+                                        Підтримка користувачів
                                     </td>
+                                    <td class="links logo" align="center" height="48" max-width="12">
+                                        support@24serpnya.com.ua
+                                    </td>
+                                </tr>
+
+                                <tr>
                                     <td class="logo" align="center" height="48" max-width="12">
                                         <a class="links" target="_blank" href="https://www.youtube.com/@24serpnya">Ми на
                                             You Tube</a>
@@ -219,8 +215,6 @@ const createSingInCode = async (req, res) => {
                                     <td class="logo" align="center" height="48" max-width="12">
                                         <a class="links" target="_blank" href="https://x.com/KabinetU60914">Ми в X</a>
                                     </td>
-                                </tr>
-                                <tr>
                                     <td class="logo" align="center" height="48" max-width="12">
                                         <a class="links" target="_blank"
                                             href="https://www.instagram.com/kabinet_gromadyanina">Ми в Instagram</a>
@@ -245,8 +239,7 @@ const createSingInCode = async (req, res) => {
         </tr>
     </table>
 </body>
-</html>
-`,
+</html>`,
   };
   transporter
     .sendMail(mail)
