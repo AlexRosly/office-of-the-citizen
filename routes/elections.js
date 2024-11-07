04b1390c044c116c6c0c54aa7296f3f12d01b841
AlexRosly <rosly.a777@gmail.com>
2024-11-07 21:38:08 +0200

    change vote route

--- routes/elections.js
+++ routes/elections.js
@@ -14,11 +14,7 @@ router.post(
 //
 router.get("/get-all-candidates", ctrlWrapper(ctrl.getAllCandidates));
 
-router.post(
-  "/vote",
-  // authCitizen,
-  ctrlWrapper(ctrl.vote)
-);
+router.post("/vote", authCitizen, ctrlWrapper(ctrl.vote));
 
 router.delete("/remove-vote", authCitizen, ctrlWrapper(ctrl.removeVote));
 
